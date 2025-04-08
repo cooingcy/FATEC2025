bbcf26dc37a093c51da420a0a34a0e32589b98df
cooingcy <matheuscurciromano@gmail.com>
2025-04-08 13:24:14 -0300

    Corrigindo o código

--- Estrutura de Dados/Atividade 8/Pilha.java
+++ Estrutura de Dados/Atividade 8/Pilha.java
@@ -1,56 +1,69 @@
 import javax.swing.JOptionPane;
 
+// Classe Pilha
 public class Pilha {
     int inicio;
     int topo = -1;
     int tamanho;
-    int qntdEelementos = 0;
-    int p[];
+    int qntdElementos = 0;
+    int p[]; // Array para armazenar os elementos da pilha
 
-    public Pilha(){
-        inicio = fim = -1;
+    // Construtor da classe Pilha
+    public Pilha() {
+        inicio = -1;
         tamanho = 10;
-        p = new int[tamanho];
-        qntdEelementos = 0;
+        p = new int[tamanho]; // Inicializa o array com o tamanho definido
+        qntdElementos = 0;
     }
 
+    // Verifica se a pilha está vazia
     public boolean estaVazia() {
-        if (qntdEelementos == 0) {
+        if (qntdElementos == 0) {
             return true;
         }
         return false;
     }
 
+    // Verifica se a pilha está cheia
     public boolean estaCheia() {
-        if (qntdEelementos == tamanho) {
+        if (qntdElementos == tamanho) {
             return true;
         }
         return false;
     }
 
-    public void empilha(int e){
-        if(!estaCheia()){
-            if(inicio == -1){
+    // Adiciona um novo elemento no topo pilha
+    public void empilha(int e) {
+        // Só empilha se a pilha ainda não estiver cheia
+        if (!estaCheia()) {
+            // Se for o primeiro elemento, define o início da pilha
+            if (inicio == -1) {
                 inicio = 0;
             }
+            // Incrementa o topo e armazena o elemento na posição correspondente
             topo++;
             p[topo] = e;
-            qntdEelementos++;
+            qntdElementos++;
         }
     }
 
-    public int desenpilha(){
-        if(!estaVazia()){
+    // Remove e retorna o elemento no topo da pilha
+    public int desempilha() {
+        // Só pode remover se não estiver vazia
+        if (!estaVazia()) {
+            // Armazena o valor do topo em uma variável temporária
             int x = p[topo];
             topo--;
-            qntdEelementos --;
+            qntdElementos--;
+            return x;
         }
-        return x;
+        return -1;
     }
 
-    public void mostrar(){
+    // Mostra todos os elementos da pilha em ordem decrescente (do topo até a base)
+    public void mostrar() {
         String elementos = "";
-        for(int i = topo; i>=0; i--){
+        for (int i = topo; i >= 0; i--) {
             elementos += p[i] + "  -  ";
         }
         JOptionPane.showMessageDialog(null, elementos);
--- Estrutura de Dados/Atividade 8/Principal.java
+++ Estrutura de Dados/Atividade 8/Principal.java
@@ -1,3 +1,19 @@
 public class Principal {
-    
+    public static void main(String[] args) {
+        Pilha teste = new Pilha();
+
+        teste.empilha(1);
+        teste.empilha(2);
+        teste.empilha(3);
+        teste.empilha(4);
+        teste.empilha(5);
+
+        teste.mostrar();
+
+        System.out.println("Índice do topo: " + teste.topo);
+
+        System.out.println("Desempilhando: " + teste.desempilha());
+
+        teste.mostrar();
+    }
 }
